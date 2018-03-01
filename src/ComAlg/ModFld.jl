144aaef8c4d2576e74da3c6b72f6e078a255a94d
Claus Fieker <fieker@murrumesh.mathematik.uni-kl.de>
2018-03-01 16:34:47 +0100

    ModField, ModEuc -> ModFree

--- src/ComAlg/ModFld.jl
+++ src/ComAlg/ModFld.jl
@@ -1,140 +1,142 @@
 #fin gen (=free) modules over fields.
 import Base: +, -, *, //, ==, div, zero, iszero, setindex!, getindex, parent, hash
 
-coeff_field(M::ModField) = M.ring
-dim(M::ModField) = M.dim
-parent(a::ModFieldElem) = a.parent
-coeff_field(a::ModFieldElem) = coeff_field(parent(a))
+coeff_field(M::ModFree{<:FieldElem}) = M.ring
+coeff_ring(M::ModFree) = M.ring
+dim(M::ModFree) = M.dim
+parent(a::ModFreeElem) = a.parent
+coeff_field(a::ModFreeElem{<:FieldElem}) = coeff_ring(parent(a))
+coeff_ring(a::ModFreeElem) = coeff_ring(parent(a))
 
-function +(a::ModFieldElem, b::ModFieldElem)
+function +(a::ModFreeElem, b::ModFreeElem)
   return a.parent(a.coeff .+ b.coeff)
 end
 
-function -(a::ModFieldElem, b::ModFieldElem)
+function -(a::ModFreeElem, b::ModFreeElem)
   return a.parent(a.coeff .- b.coeff)
 end
 
-function *(r::FieldElem, a::ModFieldElem)
+function *(r::RingElem, a::ModFreeElem)
   return a.parent(r .* a.coeff)
 end
 
-function *(r::Union{Integer, fmpz}, a::ModFieldElem)
-  return coeff_field(parent(a))(r)*a
+function *(r::Union{Integer, fmpz}, a::ModFreeElem)
+  return coeff_ring(parent(a))(r)*a
 end
 
-function *(r::Rational, a::ModFieldElem)  # cannot add fmpq - Rational is already dodgy: if
+function *(r::Rational, a::ModFreeElem)  # cannot add fmpq - Rational is already dodgy: if
                                           # the field is FlintQQ, this recurses
-  return coeff_field(parent(a))(r)*a
+  return coeff_ring(parent(a))(r)*a
 end
 
-function -(a::ModFieldElem)
+function -(a::ModFreeElem)
   return a.parent(-a.coeff)
 end
 
-==(a::ModFieldElem, b::ModFieldElem) = a.coeff == b.coeff
+==(a::ModFreeElem, b::ModFreeElem) = a.coeff == b.coeff
 
-hash(a::ModFieldElem, u::UInt) = hash(hash(a.coeff, u), UInt(17))
+hash(a::ModFreeElem, u::UInt) = hash(hash(a.coeff, u), UInt(17))
 
-function add!(a::ModFieldElem, b::ModFieldElem, c::ModFieldElem)
+function add!(a::ModFreeElem, b::ModFreeElem, c::ModFreeElem)
   a.coeff = b.coeff .+ c.coeff
 end
 
-function sub!(a::ModFieldElem, b::ModFieldElem, c::ModFieldElem)
+function sub!(a::ModFreeElem, b::ModFreeElem, c::ModFreeElem)
   a.coeff = b.coeff .- c.coeff
 end
 
-function mul!(a::ModFieldElem, r::FieldElem, b::ModFieldElem)
+function mul!(a::ModFreeElem, r::RingElem, b::ModFreeElem)
   for i=1:dim(a.parent)
     a.coeff[i] = r*b.coeff[i]
   end
 end
 
-function addeq!(a::ModFieldElem, b::ModFieldElem)
+function addeq!(a::ModFreeElem, b::ModFreeElem)
   return add!(a, a, b)
 end
 
-function iszero(a::ModFieldElem)
+function iszero(a::ModFreeElem)
   return all(iszero, a.coeff)
 end
 
-function zero(M::ModField)
+function zero(M::ModFree)
   return M(zeros(coeff_field(M), dim(M)))
 end
 
-function zero!(a::ModFieldElem)
+function zero!(a::ModFreeElem)
   for i=1:dim(a.parent)
     a.coeff[i] = 0
   end
   return a
 end
 
-function getindex(a::ModFieldElem, i::Int)
+function getindex(a::ModFreeElem, i::Int)
   return a.coeff[i]
 end
 
-function setindex!(a::ModFieldElem, i::Int, x::Union{FieldElem, Integer, fmpz})
+function setindex!(a::ModFreeElem, i::Int, x::Union{RingElem, Integer})
   a.coeff[i] = x
 end
 
-function hom(M::ModField{T}, N::ModField{T}, im::Array{ModFieldElem{T}, 1}) where T <: Nemo.FieldElem
+function hom(M::ModFree{T}, N::ModFree{T}, im::Array{ModFreeElem{T}, 1}) where T <: Nemo.RingElem
   x = []
   for y = im
     append!(x, y.coeff)
   end
-  mat = Nemo.matrix(coeff_field(M), dim(N), dim(M), x)
-  return ModFieldToModFieldMor{elem_type(coeff_field(M))}(M, N, mat)
+  mat = Nemo.matrix(coeff_ring(M), dim(N), dim(M), x)
+  return ModFreeToModFreeMor{elem_type(coeff_ring(M))}(M, N, mat)
 end
 
-function (M::ModField)(m::Nemo.MatElem)
+function (M::ModFree)(m::Nemo.MatElem)
   return M([m[i,1] for i=1:dim(M)])
 end
 
-function (M::ModField)(m::Array)
-  return M(elem_type(coeff_field(M))[x for x = m])
+function (M::ModFree)(m::Array)
+  return M(elem_type(coeff_ring(M))[x for x = m])
 end
 
-Nemo.elem_type(M::ModField{T}) where T = ModFieldElem{T}
-Nemo.elem_type(::Type{ModField{T}}) where T = ModFieldElem{T}
-Nemo.elem_type(::Type{ModField}) = ModFieldElem
+Nemo.elem_type(M::ModFree{T}) where T = ModFreeElem{T}
+Nemo.elem_type(::Type{ModFree{T}}) where T = ModFreeElem{T}
+Nemo.elem_type(::Type{ModFree}) = ModFreeElem
 
-function basis(M::ModField)
+function basis(M::ModFree)
   a = elem_type(M)[]
   for i=1:dim(M)
-    b = zeros(coeff_field(M), dim(M))
+    b = zeros(coeff_ring(M), dim(M))
     b[i] = 1
     push!(a, M(b))
   end
   return a
 end
 
-function gens(M::ModField)
+function gens(M::ModFree)
   return basis(M)
 end
 
-ngens(M::ModField) = dim(M)
+ngens(M::ModFree) = dim(M)
 
 
-function hom(M::ModField, N::ModField)
+function hom(M::ModFree, N::ModFree)
   @assert M.ring == N.ring #TODO: map to finite field??
-  return ModField(M.ring, M.dim * N.dim), hom_map
+  return ModFree(M.ring, M.dim * N.dim), hom_map
 end
 
-function sub(M::ModField{T}, a::Array{ModFieldElem{T}, 1}) where T <: Nemo.FieldElem
+function sub(M::ModFree{T}, a::Array{ModFreeElem{T}, 1}) where T <: Nemo.FieldElem
   x = []
   for y = a
     append!(x, y.coeff)
   end
-  mat = Nemo.matrix(coeff_field(M), length(a), dim(M), x)
+  mat = Nemo.matrix(coeff_ring(M), length(a), dim(M), x)
   rk = Nemo.rref!(mat)
-  S = ModField{T}(coeff_field(M), rk)
+  S = ModFree{T}(coeff_ring(M), rk)
   mat = Nemo.sub(mat, 1:rk, 1:dim(M))
-  morph = ModFieldToModFieldMor{elem_type(coeff_field(M))}(S, M, mat)
+  morph = ModFreeToModFreeMor{elem_type(coeff_ring(M))}(S, M, mat)
   S, morph
 end
 
-function quo(M::ModField, a::Array{ModFieldElem, 1})
+function quo(M::ModFree, a::Array{ModFreeElem, 1})
 end
 
-function subset(M::ModField, N::ModField)
+function subset(M::ModFree, N::ModFree)
 end
 
