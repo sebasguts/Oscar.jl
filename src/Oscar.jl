49b74b43ede7948bdb741fd7e9f477d9e9b6b6c8
William Hart <goodwillhart@googlemail.com>
2018-11-23 16:14:28 +0100

    Comment out non-working code.

--- src/Oscar.jl
+++ src/Oscar.jl
@@ -31,13 +31,13 @@ println(" ... version 0.0.2")
 println(" ... there is absolutely no warranty...")
 println("(c) 2018 by Bill Hart, Claus Fieker, Andreas Steenpass")
 
-include("ComAlg.jl")
+# include("ComAlg.jl")
 
-#to make all Map-s callable...
-for T in subtypes(Map)
-  if length(methods(T)) == 0 # to avoid re-doing the Hecke types
-    (M::T)(a) = image(M, a)
-  end  
-end
+# Make all Map-s callable...
+# for T in subtypes(Map)
+#  if length(methods(T)) == 0 # to avoid re-doing the Hecke types
+#    (M::T)(a) = image(M, a)
+#  end  
+# end
 
 end # module
